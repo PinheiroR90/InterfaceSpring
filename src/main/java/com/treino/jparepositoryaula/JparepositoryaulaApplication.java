8502735e616116115ef2bb5d14b2aa0e0413d147
RobsonPinheiro_ <74087033+PinheiroR90@users.noreply.github.com>
2022-07-06 13:03:32 -0300

    Projeto jpa

---
+++ src/main/java/com/treino/jparepositoryaula/JparepositoryaulaApplication.java
@@ -0,0 +1,13 @@
+package com.treino.jparepositoryaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JparepositoryaulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JparepositoryaulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treino/jparepositoryaula/JparepositoryaulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treino.jparepositoryaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JparepositoryaulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
